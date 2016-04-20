e69f0e7173307dd9c646e30712f7fd172d3099ff
Isaac Friedman <isaac@isaac-friedman.com>
2016-04-20 15:53:26 -0400

    different ways of printing

--- ex9.py
+++ ex9.py
@@ -6,6 +6,17 @@ print "Here are the days: ", days
 #remember the newline I told you about? This is where it happens
 print "Here are the months: ", months
 
+#printing the days and months as a formatted string gets you the same result
+print "Here are the days: %s" %days
+#remember the newline I told you about? This is where it happens
+print "Here are the months: %s" %months
+
+#but look what happens when you use raw data.
+#This is why raw data is for debugging
+print "Here are the days: %r" %days
+#remember the newline I told you about? This is where it happens
+print "Here are the months: %r" %months
+
 print '''
 This is strange
 You can type
