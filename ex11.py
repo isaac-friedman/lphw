1f97f5c5daf674f39af2ecd121e7070bd16e7707
Isaac Friedman <isaac@isaac-friedman.com>
2016-04-20 16:25:48 -0400

    introduced a bug for laughs

--- ex11.py
+++ ex11.py
@@ -6,3 +6,15 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you're %r old, %r tall, and %r heavy." % (age, height, weight)
+
+#let's learn about bugs
+print "So, you're %r old, %r tall, and %r heavy." % (weight, age, height)
+
+print """
+The above was a bug.
+Can you see what it was?
+Sure I can because I did it myself.
+What kind of a bug is it?
+Can you find it in the commit history? (This one is clearly labeled)
+Bugs are important.
+"""
