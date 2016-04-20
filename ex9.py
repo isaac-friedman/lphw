17315f0c4e06a15170a64d2a1c3e997f557896b3
Yitzchak Friedman <yitzchak@Richards-Mac-mini.local>
2016-04-20 15:46:31 -0400

    Newline char and multiline string

---
+++ ex9.py
@@ -0,0 +1,16 @@
+days = "Monday Tuesday Wednesday Thursday Friday"
+#behold the newline!
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+#remember the newline I told you about? This is where it happens
+print "Here are the months: ", months
+
+print '''
+This is strange
+You can type
+as many lines as 
+you 
+want without a newline character.
+PS you can use these for comments too but you shouldn't.
+'''
