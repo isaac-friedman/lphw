0b7cecf3ae4334f22f23bab74a18e8136cab1108
Isaac Friedman <isaac@isaac-friedman.com>
2016-04-26 17:24:57 -0400

    commented for learning

--- ex14.py
+++ ex14.py
@@ -1,17 +1,29 @@
+####
+#prompting and passing
+####
 from sys import argv
 
+#get the arguments from command line
 script, user_name, location = argv
+
+#The prompt variable is reused throughout the program
 prompt = '$ '
+
 
 print "Hi, %s! I'm the %s script." %(user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me, %s?" %user_name
+#instead of getting raw input from each question, we get it from the prompt
+#This way you can change the prompt and it changes for the whole program
 likes = raw_input(prompt)
 
 print "Where do you live, %s?" %user_name
+
+#See? Using the prompt
 lives = raw_input(prompt)
 
 print "What kind of computer do you have?"
+#and again
 computer = raw_input(prompt)
 
 if location != lives:
