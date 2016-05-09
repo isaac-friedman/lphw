02386e2862115ed53326ab46e9b80d1b69cfff09
Isaac Friedman <ifriedman21@gmail.com>
2016-05-09 16:53:09 -0400

    fixed typo and two function calls at end of file

--- ex26.py
+++ ex26.py
@@ -90,9 +90,8 @@ print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+print sorted_words
 
-print_irst_and_last(sentence)
-
-   print_first_a_last_sorted(senence)
+print_first_and_last(sentence)
+print_first_and_last_sorted(sentence)
 
