ccb7569865ef9deb5b2266188d7c2f6cc11187e6
Isaac Friedman <ifriedman21@gmail.com>
2016-05-09 17:15:39 -0400

    fixed output from calls to secret_formula

--- ex26.py
+++ ex26.py
@@ -72,12 +72,12 @@ start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 
 
 sentence = "All god \t things come to those who weight."
