68c43212634a608ba60889f34657e9e2e26031c6
Isaac Friedman <ifriedman21@gmail.com>
2016-05-10 16:53:19 -0400

    Comments.

--- ex30.py
+++ ex30.py
@@ -2,17 +2,22 @@ people = 30
 cars = 30
 trucks = 15
 
+#are there more cars than people?
 if cars > people:
 	print "We should take the cars."
+#what do you do if there are fewer?
 elif cars < people:
 	print "We should not take the cars."
+#What if there are no more nor less? What does that even mean?
 else:
 	print "We can't decide."
+
 
 if trucks > cars:
 	print "That's too many trucks."
 elif trucks < cars:
 	print "Maybe we should take the trucks."
+#There are exactly as many trucks as cars
 else:
 	print "We still can't decide."
 
