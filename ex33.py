8cad23c777441beaf66f45c85414abd505a6ac22
Isaac Friedman <ifriedman21@gmail.com>
2016-06-15 21:20:26 -0400

    exercise 33 commented

--- ex33.py
+++ ex33.py
@@ -2,11 +2,15 @@ i=0
 numbers = []
 
 while i<6:
+	#where we are at the beginning of the iteration
 	print "At the top i is %d" % i
+
 	numbers.append(i)
 
+	#if we don't increment the loop will go on forever or until the OS stops it.
 	i = i + 1
 	print "Numbers now: ", numbers
+	#now look!
 	print "At the bottom i is %d" % i
 
 print "The numbers: "
